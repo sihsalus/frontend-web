ab9b866e4eba638b130a0d561ac418aec363379c
alvax64 <a20200796@pucp.edu.pe>
2026-04-04 00:31:34 +0200

    fix: usePatient() for Servicios Especializados

--- packages/apps/esm-consulta-externa-app/src/specialized-clinics/generic-nav-links/generic-nav-links.component.tsx
+++ packages/apps/esm-consulta-externa-app/src/specialized-clinics/generic-nav-links/generic-nav-links.component.tsx
@@ -1,37 +1,36 @@
-import { ConfigurableLink, useConfig } from '@openmrs/esm-framework';
-import last from 'lodash-es/last';
-import React, { useMemo } from 'react';
-import { BrowserRouter, useLocation, useParams } from 'react-router-dom';
+import { ConfigurableLink, useConfig, usePatient } from '@openmrs/esm-framework';
+import React from 'react';
 
 import type { ConfigObject } from '../../config-schema';
 
 export const basePath = '${openmrsSpaBase}/patient/';
 
-type GenericNavLinksProps = {};
-
-const GenericNavLinks: React.FC<GenericNavLinksProps> = () => {
+const GenericNavLinks: React.FC = () => {
   const { specialClinics } = useConfig<ConfigObject>();
+  const { patientUuid } = usePatient();
+
   return (
-    <BrowserRouter>
-      {specialClinics.map((clinic) => {
-        return <GenericLink title={clinic.title} path={clinic.id} />;
-      })}
-    </BrowserRouter>
+    <>
+      {specialClinics.map((clinic) => (
+        <GenericLink key={clinic.id} title={clinic.title} path={clinic.id} patientUuid={patientUuid} />
+      ))}
+    </>
   );
 };
 
 export default GenericNavLinks;
 
-const GenericLink: React.FC<{ title: string; path: string }> = (props) => {
-  const location = useLocation();
-  const { patientUuid } = useParams<{ patientUuid: string }>();
-  const navLink = useMemo(() => decodeURIComponent(last(location.pathname.split('/'))), [location.pathname]);
+const GenericLink: React.FC<{ title: string; path: string; patientUuid: string }> = ({
+  title,
+  path,
+  patientUuid,
+}) => {
   return (
     <ConfigurableLink
       style={{ paddingLeft: '2rem' }}
       className={`cds--side-nav__link`}
-      to={`${basePath}${patientUuid}/chart/${encodeURIComponent('special-clinics-dashboard')}?clinic=${props.path}`}>
-      {props.title}
+      to={`${basePath}${patientUuid}/chart/${encodeURIComponent('special-clinics-dashboard')}?clinic=${path}`}>
+      {title}
     </ConfigurableLink>
   );
 };
