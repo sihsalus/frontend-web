825c52e42236e2b4ce6ed1d436de11bf187cc191
alvax64 <a20200796@pucp.edu.pe>
2026-04-03 02:13:26 +0200

    fix: vacunacion in sidebar

--- packages/apps/esm-vacunacion-app/src/dashboard.meta.ts
+++ packages/apps/esm-vacunacion-app/src/dashboard.meta.ts
@@ -1,8 +1,8 @@
 import { type DashboardLinkConfig } from '@openmrs/esm-patient-common-lib';
 
 export const dashboardMeta: DashboardLinkConfig & { slot: string } = {
-  slot: 'patient-chart-immunizations-dashboard-slot',
-  path: 'Immunizations',
+  slot: 'patient-chart-vacunacion-dashboard-slot',
+  path: 'Vacunacion',
   title: 'Immunizations',
   icon: 'omrs-icon-syringe',
 };
